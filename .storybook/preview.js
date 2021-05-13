2fd85ab2e4ad3d3d26b61da9dd0846162bc2d429
JC <cimbulis.j@gmail.com>
2021-05-13 08:55:34 +0300

    Show more viewports in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,7 @@
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
+
+
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
-    },
+    viewports: INITIAL_VIEWPORTS
 }
